06be2162f695d24d7dd865b7f34cbcad764dba88
Marcos Silva Chaves <156197377+MarcosSC00@users.noreply.github.com>
2026-04-30 09:19:47 -0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://relojoaria-backend.onrender.com",
-  timeout: 15000,
+  timeout: 60000,
 });
 
 export const publicApi = axios.create({
   baseURL: "https://relojoaria-backend.onrender.com",
-  timeout: 15000,
+  timeout: 60000,
 });
